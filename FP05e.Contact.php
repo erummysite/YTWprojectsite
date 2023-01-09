a23df1f9c2711842082cc849cb8cb1d423f84106
erummysite <erum.fa890@yahoo.com>
2023-01-09 12:56:24 -0500

    YTWprojectsite

---
+++ FP05e.Contact.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<title> FP05-yummytummy.html</title>
+
+<meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />    
+<meta itemprop="name" content= "15 Popular Pakistani Food Dishes You Just Can’t Ignore (with Images)" >
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" 
+href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+<style>
+body{padding:30px; background-repeat: cover; no-repeat; 
+	background-size: center;
+	background-image: url("img/CX.jpg");}
+	p{color: black;}
+	h1{font-family: 'Dancing Script', cursive;
+	   font-size: 50px;
+	   color:#e7bd42;}
+	h2{font-family: 'Dancing Script', crusive; color:#e7bd42}
+	.container {
+	background-color:#A4C639;
+	padding:30px;}
+	blockquote {
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    font-size: 17.5px;
+    border-left: 5px;
+}
+</style>
+
+<header>
+
+<!--Nav Bar-->
+<div class="container">
+
+<h1>YTW</h1>
+
+
+<ul class="nav nav-pills nav-justified" role="tablist">
+    <li <a class="active"><a href="FP05-yummytummyworld.html">Home</a></li>
+    <li><a href="FP05a-menu.html">Menu </a></li>
+    <li><a href="FP05b-Treatyourself.html">Treat yourself</a></li>
+    <li><a href="FP05c-About.html">About</a></li> 
+	<li><a href="FP05d-Contact.html">Contact Us</a></li>
+	<li><a href="FP05e-Contact.php"></a></li>
+  </ul>
+</div>
+
+</header>
+
+<div class="main-container">
+      <div class="container nopadding nomargin width100">
+        <div class="centerpage">
+          <div class="centercontent shadow">
+            <header class="topbar"><h1 class="center">CONTACT US</h1></header>
+            <div class="row">
+              <p>
+                ,<br /><br /> Thank you for your interest. I will contact you at                 within 24 business hours. I am excited to do business with you!<br /><br />Thanks again,<br /><br />Yummy Tummy World
+              </p>
+
+                          </div>
+            <footer>
+              <p></p>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
